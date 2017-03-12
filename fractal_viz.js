77d5fe9a42e0315ff8c659f4c2e89a01b4e3449a
dannyko <dannykorenblum@gmail.com>
2017-03-12 03:11:24 -0700

    working on displaying image

--- fractal_viz.js
+++ fractal_viz.js
@@ -1,54 +1,76 @@
-function grey2rgb(grey) {
-  var rgb = grey.map(function(d) {
-    var tmp = d.toString(16) ;
-    if (tmp.length === 1) {
-      tmp = '0' + tmp ;
-    }
-    return tmp ;
-  }) ;
-  return rgb ;
-}
 
-function mandelpel(imgData) {
+
+function mandelpel( imgData ) {
 
   var mandleCache = new Array( imgData.data.length / 4 ) ;
 
-  var width = sqrt( imgData.data.length / 4 ) ;
+  var width = Math. sqrt( imgData.data.length / 4 ) ;
   var scale = 3 ;
   var max   = 255 ;
+  var centerR = 0 ;
+  var centerI = 0 ;
 
-  for (let j = 0; j < imgData.data.length; j += 4) {
+  for ( var kpel = 0 ; kpel < mandleCache.length ; kpel ++ ) {
 
-    const x =  (j / 4)      % width;
-    const y = ((j / 4) - x) / width;
+    const x =  (kpel)      % width ;
+    const y = ((kpel) - x) / width ;
 
-    const r = (centerR - scale) + (x / width) * 2 * scale;
-    const i = (centerI + scale) - (y / width) * 2 * scale;
+    const r = (centerR - scale) + (x / width) * 2 * scale ;
+    const i = (centerI + scale) - (y / width) * 2 * scale ;
 
-    const incsToEscape = expandMandlebrot(r, i, max);
+    const incsToEscape = expandMandlebrot(r, i, max) ;
 
     mandleCache[kpel] = incsToEscape ;
+
   }
+
+  return mandleCache ;
 
 }
 
+function expandMandlebrot (real, imaginary, max) {
 
-const expandMandlebrot = (real, imaginary, max) => {
   var incsCount = 0;
   var r = real;
   var i = imaginary;
   var magnitude = Math.sqrt(real * real + imaginary * imaginary);
   var newReal = 0;
+  
   while(magnitude < 4.0) {
+  
     ++incsCount;
     if(incsCount > max) { return -1; }
     newReal = r * r - i * i + real;
     i = r * i * 2 + imaginary;
     r = newReal;
     magnitude = Math.sqrt(r * r + i * i);
+  
   }
-  return incsCount;
-};
+
+  return incsCount ;
+
+} ;
+
+function setgray(gray, canvas) {
+
+    img = $Z.helper.image.get_data(canvas) ;
+
+    var Npel = gray.length ;
+    var opacity = 255 ;
+
+    for (var k = 0 ; k < Npel ; k++) {
+
+      var offset = k * 4 ;
+      img.data[offset + 0] = gray[k] ;
+      img.data[offset + 1] = gray[k] ;
+      img.data[offset + 2] = gray[k] ;
+      img.data[offset + 3] = opacity ;
+
+    }
+
+    canvas.context().putImageData(img, 0, 0) ;
+
+}
 
 function fractal_viz() {
   console.log('fractal viz start') ;
@@ -62,6 +84,28 @@ function fractal_viz() {
   } ;
 
   var viz = $Z.helper.viz.setup(vizConfig) ; // frameDuration computed
+
+  var imageWidth  = canvasSize ;
+  var imageHeight = canvasSize ;
+
+  var fracImage = $Z.helper.image.create( imageWidth, imageHeight ) ;
+  var imgData = $Z.helper.image.get_data(fracImage) ;
+
+  // console.log('imgData', imgData) ;
+
+  var cache = mandelpel( imgData ) ;
+
+  // console.log(  'cache', cache, 'max', Math.max.apply(null, cache), 'min', Math.max.apply(null, cache) ) ;
+
+  setgray( cache, fracImage ) ;
+
+  var fractalItem = viz.setup_item({
+    
+    image: fracImage,
+    x: 0,
+    y: 0,
+
+  }) ;
 
   // var fractalConfig = { 
 
